ef43245e9b84bf34e2fd57952e39570e389aa68f
jcQuartic <jerry@quartic.com>
2020-06-20 13:35:28 -0400

    update main

--- main.py
+++ main.py
@@ -1,50 +1,41 @@
-import numpy as np
 import time
-
-from pensimpy.pensim_classes.Constants import H
 from pensimpy.pensim_classes.Recipe import Recipe
-from pensimpy.pensim_methods.indpensim_run import indpensim_run
+import numpy as np
+from pensimpy.pensim_methods.indpensim import indpensim
+from pensimpy.env_setup.peni_env_setup import PenSimEnv
+import math
+from pensimpy.pensim_classes.Constants import raman_wavenumber
+from pensimpy.pensim_methods.create_batch import create_batch
 
 if __name__ == "__main__":
-    recipe = Recipe()
-
     penicillin_predictions = []
     sum_intensity = np.zeros(2200)
 
     t = time.time()
-    for result in indpensim_run(recipe):
-        if result['type'] == 'raman_update':
-            pass
-            # get intensity
-            intensity = result['Intensity']
-            sum_intensity += intensity
 
-            # kth 12 minutes
-            k = result['k']
+    env = PenSimEnv()
+    xinterp, x0, H, Batch_lenght, param_list, ctrl_flags = env.reset()
+    recipe = Recipe()
 
-            # apply ML model for prediction:  penicillin = Ml(odeintensity)
-            penicillin_predictions.append(1)
+    k = 1
+    x = create_batch(H, Batch_lenght)
+    tmp = 0
+    while k != int(Batch_lenght / H) + 1:
+        x = indpensim(k, x, xinterp, x0, H, Batch_lenght, param_list, ctrl_flags, recipe)
+        peni_yield = x.V.y[k - 1] * x.P.y[k - 1] / 1000
+        print(f"=== yield: {peni_yield - tmp}")
+        tmp = peni_yield
+        k += 1
 
-            # TODO: feed intensities to model to get pensim concentration prediction
-            # TODO: send concentration prediction as well as k via websocket
-        elif result['type'] == 'batch_end':
-            # for returning final accuracy and average intensity
-            # TODO: send final accuracy and averaged intensity via websocket
+    # print(yield_sum)
 
-            # avg_intensity = sum_intensity / 1150
-            #
-            # # lower/upper bound is based on the interpolation method
-            # lower_bound = 1
-            # upper_bound = 1
-            # res = penicillin_yields[(penicillin_yields > lower_bound) & (penicillin_yields < upper_bound)]
-            # accuracy = round(len(res) / len(penicillin_yields) * 100, 2)
-            Xref = result['x']
-        else:
-            raise ValueError("Unknown flag")
+    # convert to pH from H+ concentration
+    x.pH.y = [-math.log(pH) / math.log(10) if pH != 0 else pH for pH in x.pH.y]
+    x.Q.y = [Q / 1000 for Q in x.Q.y]
+    x.Raman_Spec.Wavenumber = raman_wavenumber
 
     print(f"=== cost: {int(time.time() - t)} s")
-
-    penicillin_yield_total = (Xref.V.y[-1] * Xref.P.y[-1] - np.dot(Xref.Fremoved.y, Xref.P.y) * H) / 1000
+    penicillin_yield_total = (x.V.y[-1] * x.P.y[-1] - np.dot(x.Fremoved.y, x.P.y) * H) / 1000
     print(f"=== penicillin_yield: {penicillin_yield_total}")
 
     # # Plot the last res
