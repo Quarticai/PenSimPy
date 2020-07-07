91dd4790d64abd0fcea4835291ff7b0e2bc5a3f6
y33xiang <y33xiang@outlook.com>
2020-07-07 16:08:59 -0400

    save agent every 3000 episodes

--- run_ppo.py
+++ run_ppo.py
@@ -4,24 +4,19 @@ from pensimpy.env_setup.peni_env_setup import PenSimEnv
 import numpy as np
 import torch
 from agent import PPO
+import os.path
+import random
 
 obs_dim = 15
 act_dim = 7
-episodes = 100
+episodes_digital_twin = 3000
+episodes_production = 100
 
-
-"""time step where agent is allowed to actions"""
-agent = PPO(obs_dim, act_dim, buffer_size=1150)
-
-"""run one episode"""
-avg_yields = []
-t = time.time()
-
-for e in range(episodes):
-    env = PenSimEnv(random_seed_ref=123) # 3361 with recipe
+def learn_one_episode(seed, agent):
+    env = PenSimEnv(random_seed_ref=seed)  # 3361 with recipe
     done = False
     batch_data = env.reset()
-    o = np.zeros(obs_dim) # initial obs
+    o = np.zeros(obs_dim)  # initial obs
     recipe = Recipe()
     time_stamp, batch_yield = 0, 0
     while not done:
@@ -31,23 +26,24 @@ for e in range(episodes):
         a, v, logp = agent.ac.step(torch.as_tensor(o, dtype=torch.float32))
 
         """add adjustment to each action"""
-        Fs_a, Foil_a, Fg_a, Fpres_a, Fdischarge_a, Fw_a, Fpaa_a = list(np.clip(a/10, -0.1, 0.1))
+        Fs_a, Foil_a, Fg_a, Fpres_a, Fdischarge_a, Fw_a, Fpaa_a = list(np.clip(a / 10, -0.1, 0.1))
 
         # Get action from recipe agent based on time
         Fs, Foil, Fg, Fpres, Fdischarge, Fw, Fpaa = recipe.run(time_stamp)
 
         """update recipe actions with agent actions"""
-        Fs *= (1+Fs_a)
-        Foil *= (1+Foil_a)
-        Fg *= (1+Fg_a)
-        Fpres *= (1+Fpres_a)
-        Fdischarge *= (1+Fdischarge_a)
-        Fw *= (1+Fw_a)
-        Fpaa *= (1+Fpaa_a)
+        Fs *= (1 + Fs_a)
+        Foil *= (1 + Foil_a)
+        Fg *= (1 + Fg_a)
+        Fpres *= (1 + Fpres_a)
+        Fdischarge *= (1 + Fdischarge_a)
+        Fw *= (1 + Fw_a)
+        Fpaa *= (1 + Fpaa_a)
 
         observation, batch_data, reward, done = env.step(time_stamp,
                                                          batch_data,
-                                                         Fs, Foil, Fg, Fpres, max(Fdischarge, 0), max(Fw, 0), max(Fpaa, 0))
+                                                         Fs, Foil, Fg, Fpres, max(Fdischarge, 0), max(Fw, 0),
+                                                         max(Fpaa, 0))
 
         next_o = np.array([x[1] for x in observation])
 
@@ -58,9 +54,36 @@ for e in range(episodes):
         batch_yield += reward
     agent.buffer.finish_path()
     agent.update()
+    return batch_yield, agent
 
-    print(f"episode: {e}, elapsed time: {int(time.time() - t)} s, batch_yield: {batch_yield}")
-    avg_yields.append(batch_yield)
 
-print(np.mean(avg_yields))
-# 3531
+def main():
+
+    for i in range(4):
+        """time step where agent is allowed to actions"""
+        if os.path.isfile('agent.pt'):
+            agent = torch.load('agent.pt')
+        else:
+            agent = PPO(obs_dim, act_dim, buffer_size=1150)
+
+        """run one episode"""
+        avg_yields = []
+        t = time.time()
+
+        for e in range(episodes_digital_twin):
+            batch_yield, agent = learn_one_episode(random.randint(1, 1000), agent)
+            print(f"digital twin episode: {e}, elapsed time: {int(time.time() - t)} s, digital twin batch_yield: {batch_yield}")
+            avg_yields.append(batch_yield)
+        print(np.mean(avg_yields))
+
+        torch.save(agent, 'agent.pt')
+
+
+        for e in range(episodes_production):
+            batch_yield, agent = learn_one_episode(274, agent)
+            print(f"production episode: {e}, elapsed time: {int(time.time() - t)} s, production batch_yield: {batch_yield}")
+            avg_yields.append(batch_yield)
+        print(np.mean(avg_yields))
+
+if __name__ == '__main__':
+    main()
