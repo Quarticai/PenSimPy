1d2006aa0cbc6b08bf332903d04964ced39312c7
jerry <jerry@jerrydeimac.lan>
2020-08-04 11:28:06 -0400

    add example

---
+++ pensimpy/examples/batch_generation_example.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../..")
+from pensimpy.env_setup.peni_env_setup import PenSimEnv
+
+
+# Provide recipes for Fs, Foil, Fg, pres, discharge, water
+# Recipe time range: 0 < t <= 230
+setpoints_dict = {'water': [(100, 11)]}
+
+env = PenSimEnv()
+df, df_raman = env.get_batches(random_seed=1, setpoints=setpoints_dict, include_raman=False)
