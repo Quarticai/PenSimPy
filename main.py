89b87bed3ee8ecfe68c1d24a87a0f66b724cc8a4
jcQuartic <jerry@quartic.com>
2020-08-06 09:58:13 -0400

    remove main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from pensimpy.peni_env_setup import PenSimEnv
-from pensimpy.data.recipe import Recipe
-
-if __name__ == "__main__":
-    # Provide recipes for Fs, Foil, Fg, pres, discharge, water
-    # Recipe time range: 0 < t <= 230
-    recipe = Recipe.get_default()
-    env = PenSimEnv(recipe=recipe)
-    df, df_raman = env.get_batches(random_seed=1, include_raman=False)
